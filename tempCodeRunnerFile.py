7f1897b778bfa71c9191d0dbf4a12b4d9581289e
Sumaiya_Zoha <sumaiyarodela081.email@example.com>
2025-08-05 12:41:38 +0600

    initial commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,39 @@
 import random 
+rock='''
+rock      /---]
+--------[_____]
+--------[_____]
+--------[_____]
+--------[_____]
+        \____]
+'''
+paper= '''
+paper   /_|_|]
+--------[__|___|__]
+--------[__|___|__]
+--------[__|___|__]
+--------[__|___|__]
+        [_|__|__]
+'''
+
+scissors='''
+scissors /]
+--------/__|__|__)
+--------)
+--------\___|__|__)
+--------[___]
+--------\__]
+        
+'''
+game_image=[rock,paper,scissors]
 user= int(input("enter your choice (0 for rock, 1 for paper and 2 for Scissor): "))
 if user>=3 or user<0 :
     print("invalid number . YOU LOSS")
 else:
+    print(game_image[user])
     computer= random.randint(0,2)
     print("computer choose:")
+    print(game_image[computer])
     print(computer)
 
     if computer==user :
